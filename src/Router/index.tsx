b59907d6c1725bdb44155705eb2dc1d55ce80920
Werbeth Araujo <werbeth1991pbs@gmail.com>
2025-01-03 22:37:50 -0300

    feat: created pages login register and dashboard

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,17 +1,20 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { AuthGuard } from "./AuthGuard";
+import { Login } from "../view/pages/Login";
+import { Register } from "../view/pages/Register";
+import { Dashborad } from "../view/pages/Dashboard";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AuthGuard isPrivate={false} />}>
-          <Route path="/login" element={<h1>Login</h1>} />
-          <Route path="/register" element={<h1>Register</h1>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
 
         <Route element={<AuthGuard isPrivate />}>
-          <Route path="/" element={<h1>Dashborad</h1>} />
+          <Route path="/" element={<Dashborad />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/view/pages/Dashboard/index.tsx
@@ -0,0 +1,3 @@
+export function Dashborad() {
+  return <h1>Dashboard</h1>;
+}
