100605197ff21f3b0678f07e6445300e9c572cb6
Werbeth Araujo <werbeth1991pbs@gmail.com>
2025-01-05 23:00:23 -0300

    feat: created login form and input component

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -5,13 +5,13 @@ import { Logo } from "../components/Logo";
 export function AuthLayout() {
   return (
     <div className="flex w-full h-full">
-      <div className=" w-1/2 h-full flex flex-col justify-center items-center gap-16">
+      <div className=" w-full h-full flex flex-col justify-center items-center gap-16 lg:w-1/2">
         <Logo className="text-gray-500 h-8" />
-        <div className="w-full max-w-md">
+        <div className="w-full max-w-[504px] px-8">
           <Outlet />
         </div>
       </div>
-      <div className="w-1/2 h-full flex justify-center items-center p-8 relative">
+      <div className="w-1/2 h-full justify-center items-center p-8 relative hidden lg:flex">
         <img
           src={illustration}
           alt="banner login and register page"
