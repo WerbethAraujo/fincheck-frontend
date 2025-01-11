ec8955049e9e1a56eb888374446f943b36421505
Werbeth Araujo <werbeth1991pbs@gmail.com>
2025-01-11 00:08:43 -0300

    feat: form fields validated with zod

--- src/view/pages/Login/useLoginController.ts
+++ src/view/pages/Login/useLoginController.ts
@@ -1,11 +1,32 @@
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .nonempty("E-mail é obrigatório!")
+    .email("Informe um e-mail válido!"),
+  password: z
+    .string()
+    .nonempty("A senha não pode ser vazia!")
+    .min(8, "Sua senha precisa ter no mínimo 8 caracteres!"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function useLoginController() {
-  const { handleSubmit: hookFormHandleSubmit, register } = useForm();
-
-  const handleSubmit = hookFormHandleSubmit((data) => {
-    console.log({ data });
+  const {
+    register,
+    handleSubmit: hookFormHandleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
   });
 
-  return { handleSubmit, register };
+  const handleSubmit = hookFormHandleSubmit((data) => {
+    console.log("Passou foi tudo 🔥", data);
+  });
+
+  return { handleSubmit, register, errors };
 }
