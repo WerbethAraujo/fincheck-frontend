dce61085633b984a5984a089211afd0a904320eb
Werbeth Araujo <werbeth1991pbs@gmail.com>
2025-02-07 23:56:50 -0300

    feat: implementing the account logout dropdown

--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,9 +1,29 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+import { DropdownMenu } from "./DropdownMenu";
+import { useAuth } from "../../app/hooks/useAuth";
+
 export function UserMenu() {
+  const { signout } = useAuth();
+
   return (
-    <div className="flex items-center justify-center bg-teal-50 rounded-full w-12 h-12 border border-teal-200">
-      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
-        WA
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="flex items-center justify-center bg-teal-50 rounded-full w-12 h-12 border border-teal-200">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+            WA
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32">
+        <DropdownMenu.Item
+          onSelect={signout}
+          className="flex items-center justify-between"
+        >
+          Sair
+          <ExitIcon />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
--- src/view/pages/Dashboard/components/transactions/index.tsx
+++ src/view/pages/Dashboard/components/transactions/index.tsx
@@ -13,18 +13,20 @@ import { Spinner } from "../../../../components/Spinner";
 import emptyStateImage from "../../../../../assets/empty-state.svg";
 
 export function Transactions() {
-  const { areValuesVisible, isLoading, transactions } =
+  const { areValuesVisible, isInitialLoading, isLoading, transactions } =
     useTransactionsController();
+
+  const hasTransactions = transactions.length > 0;
 
   return (
     <div className="flex flex-col bg-gray-100 rounded-2xl w-full h-full md:p-10 px-4 py-8">
-      {isLoading && (
+      {isInitialLoading && (
         <div className="w-full h-full flex items-center justify-center">
           <Spinner />
         </div>
       )}
 
-      {!isLoading && (
+      {!isInitialLoading && (
         <>
           <header>
             <div className=" flex items-center justify-between">
@@ -59,15 +61,21 @@ export function Transactions() {
             </div>
           </header>
           <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
-            {transactions.length === 0 && (
-              <div>
-                <div>
-                  <img src={emptyStateImage} alt="Empty state image" />
-                  <p>Nao encontramos nenhuma transação!</p>
-                </div>
+            {isLoading && (
+              <div className="flex flex-col items-center justify-center h-full">
+                <Spinner />
               </div>
             )}
-            {transactions.length > 0 && (
+
+            {!hasTransactions && !isLoading && (
+              <div className="flex flex-col justify-center items-center h-full">
+                <img src={emptyStateImage} alt="Empty state image" />
+                <p className="text-gray-700">
+                  Nao encontramos nenhuma transação!
+                </p>
+              </div>
+            )}
+            {hasTransactions && !isLoading && (
               <>
                 <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
                   <div className="flex flex-1 items-center gap-3">
